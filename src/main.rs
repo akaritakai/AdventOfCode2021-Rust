b4269b611582bf3f03950619f5b952119df80dca
Justin Kaufman <akaritakai@gmail.com>
2021-12-06 11:29:26 -0600

    Add day 6 solution

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crate::puzzle02::Puzzle02;
 use crate::puzzle03::Puzzle03;
 use crate::puzzle04::Puzzle04;
 use crate::puzzle05::Puzzle05;
+use crate::puzzle06::Puzzle06;
 use crate::puzzle_input_fetcher::PuzzleInputFetcher;
 
 mod puzzle;
@@ -12,6 +13,7 @@ mod puzzle02;
 mod puzzle03;
 mod puzzle04;
 mod puzzle05;
+mod puzzle06;
 mod puzzle_input_fetcher;
 
 fn main() {
@@ -22,6 +24,7 @@ fn main() {
         Puzzle03::create(fetcher.fetch_puzzle_input(3).unwrap()),
         Puzzle04::create(fetcher.fetch_puzzle_input(4).unwrap()),
         Puzzle05::create(fetcher.fetch_puzzle_input(5).unwrap()),
+        Puzzle06::create(fetcher.fetch_puzzle_input(6).unwrap()),
     ];
     for puzzle in puzzles.iter() {
         let day = format!("{:02}", puzzle.get_day());
---
+++ src/puzzle06.rs
@@ -0,0 +1,80 @@
+use crate::puzzle::AbstractPuzzle;
+
+pub struct Puzzle06 {
+    input: String,
+}
+
+impl AbstractPuzzle for Puzzle06 {
+    fn get_day(&self) -> u8 {
+        6
+    }
+
+    fn solve_part_1(&self) -> String {
+        simulate(self.input.as_str(), 80).to_string()
+    }
+
+    fn solve_part_2(&self) -> String {
+        simulate(self.input.as_str(), 256).to_string()
+    }
+}
+
+impl Puzzle06 {
+    pub fn create(input: &str) -> Box<dyn AbstractPuzzle> {
+        Box::new(Puzzle06 {
+            input: input.to_string(),
+        })
+    }
+}
+
+fn simulate(input: &str, days: usize) -> u64 {
+    let mut fish: [u64; 9] = [0; 9];
+    input
+        .trim()
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .for_each(|x| fish[x] += 1);
+    for _ in 0..days {
+        let new_fish = fish[0];
+        for i in 0..8 {
+            fish[i] = fish[i + 1];
+        }
+        fish[6] += new_fish;
+        fish[8] = new_fish;
+    }
+    fish.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzle06::Puzzle06;
+    use std::fs;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_part_1_example_1() {
+        let input = "3,4,3,1,2";
+        let puzzle = Puzzle06::create(&input);
+        assert_eq!(puzzle.solve_part_1(), "5934");
+    }
+
+    #[test]
+    fn test_solve_part_1() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/6")).unwrap();
+        let puzzle = Puzzle06::create(&input);
+        assert_eq!(puzzle.solve_part_1(), "349549");
+    }
+
+    #[test]
+    fn test_part_2_example_1() {
+        let input = "3,4,3,1,2";
+        let puzzle = Puzzle06::create(&input);
+        assert_eq!(puzzle.solve_part_2(), "26984457539");
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/6")).unwrap();
+        let puzzle = Puzzle06::create(&input);
+        assert_eq!(puzzle.solve_part_2(), "1589590444365");
+    }
+}
