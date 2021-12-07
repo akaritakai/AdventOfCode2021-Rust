c030cfb73e0ce8e1b4b999d6d82b90186b668594
Justin Kaufman <akaritakai@gmail.com>
2021-12-07 09:31:16 -0600

    Algorithm improvements for day 7

--- src/puzzle07.rs
+++ src/puzzle07.rs
@@ -3,8 +3,6 @@ use std::cmp::min;
 
 pub struct Puzzle07 {
     positions: Vec<i32>,
-    min_position: i32,
-    max_position: i32,
 }
 
 impl AbstractPuzzle for Puzzle07 {
@@ -13,45 +11,43 @@ impl AbstractPuzzle for Puzzle07 {
     }
 
     fn solve_part_1(&self) -> String {
-        let mut min_cost = i32::MAX;
-        for i in self.min_position..=self.max_position {
-            let mut cost = 0;
-            for position in self.positions.iter() {
-                cost += (position - i).abs();
-            }
-            min_cost = min(min_cost, cost);
+        let median = self.positions[self.positions.len() / 2];
+        let mut cost = 0;
+        for position in self.positions.iter() {
+            cost += (position - median).abs();
         }
-        min_cost.to_string()
+        cost.to_string()
     }
 
     fn solve_part_2(&self) -> String {
-        let mut min_cost = i32::MAX;
-        for i in self.min_position..=self.max_position {
-            let mut cost = 0;
-            for position in self.positions.iter() {
-                let distance = (position - i).abs();
-                cost += distance * (distance + 1) / 2;
-            }
-            min_cost = min(min_cost, cost);
+        let mut mean = 0 as f64;
+        for position in self.positions.iter() {
+            mean += *position as f64;
         }
-        min_cost.to_string()
+        mean /= self.positions.len() as f64;
+        let mut floor_cost = 0;
+        let mut ceil_cost = 0;
+        for position in self.positions.iter() {
+            let floor = mean.floor() as i32;
+            let floor_distance = (position - floor).abs();
+            floor_cost += floor_distance * (floor_distance + 1) / 2;
+            let ceil = mean.ceil() as i32;
+            let ceil_distance = (position - ceil).abs();
+            ceil_cost += ceil_distance * (ceil_distance + 1) / 2;
+        }
+        min(floor_cost, ceil_cost).to_string()
     }
 }
 
 impl Puzzle07 {
     pub fn create(input: &str) -> Box<dyn AbstractPuzzle> {
-        let positions = input
+        let mut positions = input
             .trim()
             .split(',')
             .map(|x| x.parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
-        let min_position = *positions.iter().min().unwrap();
-        let max_position = *positions.iter().max().unwrap();
-        Box::new(Puzzle07 {
-            positions,
-            min_position,
-            max_position,
-        })
+        positions.sort_unstable();
+        Box::new(Puzzle07 { positions })
     }
 }
 
